0a8ad684274fcfd689b1d1084683625f4d366d95
Visheshkumar <visheshkumar95647@gmail.com>
2025-05-29 10:26:33 +0530

    build

--- src/URL.js
+++ src/URL.js
@@ -1,2 +1,1 @@
-export const baseURL = `http://localhost:5000`;
-// export const baseURL = `https://expense-tracker-backend-2-oodc.onrender.com`;
+export const baseURL = `https://expense-tracker-backend-2-oodc.onrender.com`;
